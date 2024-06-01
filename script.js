fb47c3e5bd7de8fdf91c252a820f9a77db11bab6
Martin Conti <matin.a.conti2014@gmail.com>
2024-06-01 18:49:04 -0300

    projects section ,media 600px restyling in cards

--- script.js
+++ script.js
@@ -7,26 +7,9 @@ const mail = document.getElementById("email");
 const message = document.getElementById("message");
 
 function errorHandler(value) {
-	// value.nextElementSibling.textContent = "Fill out the field...";
 	value.classList.remove("rm-error");
 	value.classList.add("error");
 }
-
-// submit function to fill with backend !!!!
-
-// submitForm.addEventListener("submit", (event) => {
-// 	event.preventDefault();
-// 	console.log("I am here");
-// 	n.value = "";
-// 	errorHandler(n);
-// 	subject.value = "";
-// 	errorHandler(subject);	
-// 	mail.value = "";
-// 	errorHandler(mail);
-// 	message.value = "";
-// 	errorHandler(message);
-// 	event.submitForm()
-// });
 
 const camposFormulario = document.querySelectorAll("[required]");
 
@@ -37,8 +20,6 @@ camposFormulario.forEach((campo) => {
 
 function verificarCampor(campo) {
 	if (campo.validity.valueMissing == false && campo.validity.typeMismatch == false) {
-		// const errorMenssage = campo.nextElementSibling;
-		// errorMenssage.textContent = "";
 		campo.classList.remove("error");
 		campo.classList.add("rm-error");
 		console.log(campo.validity);
